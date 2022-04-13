d04dcafa399a36bd19d60b931b6ff08766239648
IamLakhan <gurbanilakhan7@gmail.com>
2022-04-13 12:26:39 +0530

    problem is still there

--- connect.js
+++ connect.js
@@ -1,21 +1,4 @@
-import oracledb from "oracledb"
 
-async function connect() {
-
-    let connection;
-  
-    try {
-      connection = await oracledb.getConnection( {
-        user          : "c##aduser",
-        password      : "root",
-        connectString : "localhost:1521/xe"
-      });
-    }
-    catch(e){
-        console.log(e)
-    }
-    return connection
-}
 
 
 // run()
--- server.js
+++ server.js
@@ -1,16 +1,40 @@
+import oracledb from "oracledb"
 import express from "express"
 const app = express()
-import connect from "./connect.js"
 
-async function run(){
-const con = connect()
-// console.log(con)
-try{
-// const result = con.execute(`select * from batch1btr_user`)
+app.listen(1337, ()=>{
+    console.log("listening at 1337")
+})
 
-const result = await con.execute(`select * from emp`)
-console.log(result.rows)
-}
-catch(e){console.log(e)}
-}
-run()
+app.get("/", (req,res)=>{
+    const query = "select * from emp"
+    res.send(connect(query))
+})
+
+async function connect(query) {
+
+    let connection;
+  
+    try {
+      connection = await oracledb.getConnection( {
+        user          : "c##aduser",
+        password      : "root",
+        connectString : "localhost:1521/xe"
+      });
+      connection.execute(
+        `SELECT *
+         FROM emp`,
+        [],  
+       function(err, result) {
+          if (err) {
+            console.error(err.message);
+            return;
+          }
+          console.log(result.rows);
+          return result.rows
+       });
+       
+       }
+       catch(e){
+           console.log(e)
+       }}
